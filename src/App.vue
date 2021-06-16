0f41a456731a0e1cd0b0d03ad0dab95d942a64e7
undefined <undefined>
2021-06-16 12:43:34 +0800

    opt:更新Dependencies

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ export default {
   name: 'App',
   data() {
     return {
-      jsonData: '{ "creator": { "creator1": 123, "creator2": [ { "creator21": 234 } ], "creator3": 456 }, "dataSwitch": { "dataSwitch1": 234 }, "dataSwitchCn": "开", "eventDesc": "Click", "eventId": 289, "eventName": "Click", "hasReportedData": true, "hasReportedDataCn": "是", "isHide": false, "isHideCn": "显示", "lastEditTime": "2021-06-09 15:04:19", "lastEditor": "王伟", "lastEditorUserId": "4378", "metaEventType": "preset", "metaEventTypeCn": "预置", "projectId": 2, "recentlyDataNum": 0, "remark": "", "status": "display", "statusCn": "正常" }',
+      jsonData: '{ "creator": { "creator1": 123, "creator2": [ { "creator21": 234 } ], "creator3": 456 }, "dataSwitch": { "dataSwitch1": 234 }, "dataSwitchCn": "开", "eventDesc": "Click", "eventId": 289, "eventName": "Click", "hasReportedData": true, "hasReportedDataCn": "是", "isHide": false, "isHideCn": "显示", "lastEditTime": "2021-06-09 15:04:19", "lastEditor": "test1", "lastEditorUserId": "4378", "metaEventType": "preset", "metaEventTypeCn": "preset", "projectId": 2, "recentlyDataNum": 0, "remark": "", "status": "display", "statusCn": "normal" }',
     };
   },
   methods: {
